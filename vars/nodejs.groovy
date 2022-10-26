a8548349af1c07ea9daee76601d2b4029c96ac96
venkat <venkymec1922@gmail.com>
2022-10-26 16:13:46 +0530

    sample branch demo

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,7 +8,7 @@ def call() {
         sh 'env'
         common.codeQuality()
 
-        if ( BRANCH_NAME == "main" || TAG_NAME ==~ "*" ) {
+        if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
             stage('Style Checks') {
                 echo 'Style Checks'
             }
