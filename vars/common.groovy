c4c5d3566b97e4fc14e57ca69e06c751ad3064a5
venkat <venkymec1922@gmail.com>
2022-10-26 16:30:11 +0530

    sample branch demo

--- vars/common.groovy
+++ vars/common.groovy
@@ -3,3 +3,35 @@ def codeQuality() {
         echo 'Code Quality'
     }
 }
+
+def codeChecks() {
+    if ( env.BRANCH_NAME == "main" || env.TAG_NAME ==~ ".*" ) {
+
+        stage('Style Checks') {
+            echo 'Style Checks'
+        }
+
+        stage('Unit Tests') {
+            echo 'Unit Tests'
+        }
+
+    }
+}
+
+def artifacts() {
+    if ( env.TAG_NAME ==~ ".*" ) {
+
+        stage('Download Dependencies') {
+            echo 'Download Dependencies'
+        }
+
+        stage('Prepare Artifacts') {
+            echo 'Prepare Artifacts'
+        }
+
+        stage('Publish Artifacts') {
+            echo 'Publish Artifacts'
+        }
+
+    }
+}
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,17 +1,13 @@
 def call() {
     node {
 
-        if (! env.TAG_NAME) {
+        if (!env.TAG_NAME) {
             env.TAG_NAME = ""
         }
 
-        sh 'env'
         common.codeQuality()
-
-        if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
-            stage('Style Checks') {
-                echo 'Style Checks'
-            }
-        }
+        common.codeChecks()
+        common.artifacts()
     }
+
 }
