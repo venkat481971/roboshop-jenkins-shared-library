32bd729f1dd07d602313d65e58eef1ba2a5a626c
venkat <venkymec1922@gmail.com>
2022-10-26 23:24:45 +0530

    sample branch demo

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,9 +1,12 @@
 def call() {
     node {
-
-        common.codeQuality()
-        common.codeChecks()
-        common.artifacts()
+        try {
+            common.codeQuality()
+            common.codeChecks()
+            common.artifacts()
+        }   catch (Exception e) {
+            mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'venkymec1922@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'venkymec1922@gmail.com'
+        }
     }
 
 }
