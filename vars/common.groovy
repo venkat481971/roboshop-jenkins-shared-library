fb2e89788816c79c73f902b4ab99a78b380ee7fc
venkat <venkymec1922@gmail.com>
2022-10-28 16:24:11 +0530

    sample branch demo

--- vars/common.groovy
+++ vars/common.groovy
@@ -33,12 +33,31 @@ def codeChecks() {
 def artifacts() {
     if ( env.TAG_NAME ==~ ".*" ) {
 
-        stage('Download Dependencies') {
-            echo 'Download Dependencies'
-        }
-
         stage('Prepare Artifacts') {
-            echo 'Prepare Artifacts'
+            if (env.APPTYPE == "nodejs") {
+                sh '''
+          npm install 
+          zip -r ${COMPONENT}-${TAG_NAME}.zip node_modules server.js 
+        '''
+            }
+            if (env.APPTYPE == "java") {
+                sh '''
+          mvn clean package 
+          mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar 
+          zip -r ${COMPONENT}-${TAG_NAME}.zip ${COMPONENT}.jar
+        '''
+            }
+            if (env.APPTYPE == "python") {
+                sh '''
+          zip -r ${COMPONENT}-${TAG_NAME}.zip *.py ${COMPONENT}.ini requirements.txt
+        '''
+            }
+            if (env.APPTYPE == "nginx") {
+                sh '''
+          cd static
+          zip -r ../${COMPONENT}-${TAG_NAME}.zip *
+        '''
+            }
         }
 
         stage('Publish Artifacts') {
